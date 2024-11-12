d706903adf1b85517cf1c2038d80c1b041cf52b8
Elvis Kuria <levonjoroge>
2024-11-12 10:07:18 +0300

    add

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -16,10 +16,10 @@ function PlantList() {
   }
   
   return (
-    <div> 
+    <div className="row "> 
       {plants.map((plant) => (
         
-          <div>
+          <div  className="col-6 col-sm-6 col-md-4 col-lg-3 mb-4">
         <PlantCard   key={plant.id} plant={plant} />
         </div>
 
